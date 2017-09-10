827691c7370b1fca29150ee5f18c4961cd190e79
Ruslan Isaev <ubijca16@gmail.com>
2017-09-10 09:55:05 +0000

    First commit
    This commit adds:
    	- Framed button

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', function(){
+	return gulp.src('./scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css'));
+});
+
