eeb367128ecef1c3ed982a50cf535ddc5390d464
Ruslan Isaev <rusikisaev@list.ru>
2018-03-24 10:03:14 +0300

    Update gulpfile.js
    If gulp finds css files in scss directory, it will copy them into css directory.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('default', function(){
-	return gulp.src('./scss/**/*.scss')
+	gulp.src('./scss/**/*.css')
+		.pipe(gulp.dest('./css'));
+	gulp.src('./scss/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./css'));
 });
